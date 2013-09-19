712c187ac9ec622a2dbd8de5049eb4f536f3c460
pia-negandhi <pia.srivastava@cognitivematch.com>
2013-09-19 00:06:25 -0700

    Adding in all files

---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Pia on 9/17/13.
+//  Copyright (c) 2013 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
